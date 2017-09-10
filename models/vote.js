dba889b2e8cc33eebfa78549701139dec9d3df92
Michael P <compte@paqman.fr>
2017-09-11 00:12:41 +1000

    Return old votes into poll

--- models/vote.js
+++ models/vote.js
@@ -2,6 +2,12 @@ const INSERT_OR_UPDATE_VOTE = `INSERT OR REPLACE INTO
 vote(idUser, idCriteria, idItem, rate, creationDate)
 VALUES ($idUser, $idCriteria, $idItem, $rate, datetime())`;
 
+const SELECT_VOTES = `SELECT v.*
+FROM vote v, item i
+WHERE i.idPoll = $idPoll
+AND v.idUser = $idUser
+AND v.idItem = i.idItem`;
+
 function addOrUpdateVote(db, idUser, idCriteria, idItem, rate) {
   return new Promise((resolve, reject) => {
 
@@ -23,6 +29,29 @@ function addOrUpdateVote(db, idUser, idCriteria, idItem, rate) {
   });
 }
 
+/**
+ * Get the votes of a user for a poll
+ * @param db
+ * @param idPoll
+ * @param idUser
+ * @returns {Promise}
+ */
+function getPollVotes(db, idPoll, idUser) {
+  return new Promise((resolve, reject) => {
+    const params = {'$idPoll': idPoll, '$idUser': idUser};
+
+    db.all(SELECT_VOTES, params, (err, rows) => {
+      if (err) {
+        return reject(err);
+      }
+
+      console.log(`found ${rows.length} votes.`);
+      return resolve(rows);
+    });
+  });
+}
+
 module.exports = {
-  addOrUpdateVote
+  addOrUpdateVote,
+  getPollVotes
 };
--- routes/poll.js
+++ routes/poll.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const pollModel = require('../models/poll');
 const itemModel = require('../models/item');
+const voteModel = require('../models/vote');
 
 const router = express.Router();
 
@@ -13,9 +14,21 @@ router.get('/', function (req, res, next) {
     .then(poll => {
       const pItems = itemModel.getPollItems(_db, poll.idPoll);
       const pCriterias = itemModel.getPollCriterias(_db, poll.idPoll);
+      const pVotes = voteModel.getPollVotes(_db, poll.idPoll, res.locals.user);
 
-      Promise.all([pItems, pCriterias])
-        .then(([items, criterias]) => {
+      Promise.all([pItems, pCriterias, pVotes])
+        .then(([items, criterias, votes]) => {
+          items.forEach(item => {
+            item.criterias = criterias.map(criteria => {
+              const vote = votes.find(vote => vote.idCriteria === criteria.idCriteria && vote.idItem === item.idItem);
+              if (vote !== undefined) {
+                criteria.rate = vote.rate;
+                criteria.vote = vote;
+              }
+              return criteria;
+            });
+          });
+
           res.send({poll, items, criterias});
         })
     });
